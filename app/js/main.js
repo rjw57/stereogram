4ce87295f32a521e2009d1b8785fe8f2f028aabb
Rich Wareham <rjw57@cam.ac.uk>
2013-12-06 17:18:36 +0000

    add selectable backgrounds

--- app/js/main.js
+++ app/js/main.js
@@ -1,20 +1,47 @@
 /* jshint strict: true */
 
-require(['lib/magiceye'], function(magiceye) {
+require(['lib/magiceye', 'jquery', 'bootstrap'], function(magiceye, $, _) {
   'use strict';
 
-  var depthImg = document.getElementById('depth-image');
-  var textureImg = document.getElementById('texture-image');
-  var magicEyeCanvas = document.getElementById('magic-eye');
+  var makeTexClickFunc = function(group) { return (function(event) {
+    // remove 'active' from the currently selected texture
+    var activeTex = group.find('.active').removeClass('active');
 
-  // Called when the depth and texture images change loading state. Checks to
-  // see if images have been loaded and, if so, updates the magic eye canvas.
-  var imgLoaded = function() {
+    // add 'active' to the clicked texture
+    $(this).addClass('active');
+
+    // stop the event bubbling
+    event.preventDefault();
+
+    // update stereogram
+    updateStereogram();
+  }); };
+
+  // Wire up events for all textures
+  $('[data-group="texture"]').each(function() {
+    var textureGroup = $(this);
+    textureGroup.find('[data-texture]').each(function() {
+      $(this).click(makeTexClickFunc(textureGroup));
+    });
+  });
+
+  // Called when the document is loaded or when background or depth image changes
+  var updateStereogram = function() {
+    var depthImg = $('#depth-image').get(0);
+    var textureImg = $('[data-group="texture"]').first().find('.active .texture').get(0);
+    var stereogramImg = $('#stereogram-image').get(0);
+
+    console.log('hello', depthImg, textureImg, stereogramImg);
+
+    if(!depthImg || !textureImg || !stereogramImg) return;
     if(!depthImg.complete || !textureImg.complete) return;
-    console.log('Loading complete');
-    magiceye.updateMagicEye(magicEyeCanvas, depthImg, textureImg);
+
+    console.log('Updating stereogram');
+    var img = magiceye.updateMagicEye(depthImg, textureImg);
+    stereogramImg.src = img.src;
   };
 
-  depthImg.onload = textureImg.onload = imgLoaded;
-  imgLoaded();
+  // Update the initial stereogram when the DOM is ready and/or when all images are loaded
+  $(document).ready(updateStereogram);
+  $(document).load(updateStereogram);
 });
