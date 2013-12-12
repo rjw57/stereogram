a3632b38c87dea542608da4410dac100d7fcd485
Rich Wareham <rjw57@cam.ac.uk>
2013-12-12 18:11:10 +0000

    restore texture group selection functionality

--- app/scripts/directives/texturegroup.js
+++ app/scripts/directives/texturegroup.js
@@ -3,24 +3,49 @@
 angular.module('stereogramApp')
   .directive('texturegroup', function() {
     return {
-      scope: { },
+      scope: { ngModel: '=?' },
       restrict: 'EA',
       template:
         '<div class="texture-group container">' +
           '<div class="row" ng-transclude></div>' +
         '</div>',
       transclude: true,
+      controller: function($scope) {
+        var textures = $scope.textures = [];
+
+        var select = function(tex) {
+          angular.forEach(textures, function(t) {
+            t.selected = false;
+          });
+          tex.selected = true;
+          $scope.ngModel = tex.src;
+        };
+
+        this.addTexture = function(tex) {
+          if((textures.length === 0) || tex.selected) { select(tex); }
+          textures.push(tex);
+          tex.$watch('selected', function(v) {
+            if(v) { select(tex); }
+          });
+        };
+      },
     };
   })
   .directive('texture', function() {
     return {
       scope: { src: '@' },
       restrict: 'EA',
+      require: '^texturegroup',
       template:
         '<div class="texture" ng-class="{selected: selected}">' +
           '<a href="" ng-click="selected = true"><div class="texture-frame">' +
             '<div class="texture-swatch" style="background-image: url({{ src }})">' +
           '</div></a>' +
         '</div>',
+      link: function(scope, element, attrs, textureGroupCtrl) {
+        // initialise selected flag
+        scope.selected = attrs.selected;
+        textureGroupCtrl.addTexture(scope);
+      },
     };
   });
--- test/spec/directives/texturegroup.js
+++ test/spec/directives/texturegroup.js
@@ -1,6 +1,6 @@
 'use strict';
 
-describe('directive: textures', function() {
+describe('directive: texturegroup', function() {
   var $compile;
   var $rootScope;
 
@@ -15,9 +15,65 @@ describe('directive: textures', function() {
     $rootScope = _$rootScope_;
   }));
 
-  it('replaces <texturegroup> element with a div with texture-group class', function() {
+  it('replaces texturegroup element with a div with texture-group class', function() {
     var element = $compile('<div texturegroup></div>')($rootScope);
-    console.log(element);
     expect(element.html()).toMatch('class="[^"]*texture-group[^"]*"');
+  });
+
+  it('replaces texture element with a div with texture-frame and src', function() {
+    var element = $compile('<div texturegroup><div texture src="fooBarBuzz"></div>')($rootScope);
+    expect(element.html()).toMatch('class="[^"]*texture-frame[^"]*"');
+    expect(element.html()).toContain('fooBarBuzz');
+  });
+
+  it('add empty list of textures to scope when no child textures specified', function() {
+    var element = $compile('<div texturegroup></div>')($rootScope);
+    $rootScope.$digest();
+    expect(element.isolateScope().textures.length).toBe(0);
+  });
+
+  it('add list of textures to scope corresponding to textures', function() {
+    var element = $compile(
+      '<div texturegroup>'+
+        '<texture src="foo"></texture><texture src="bar"></texture>' +
+      '</div>')($rootScope);
+    $rootScope.$digest();
+
+    var textures = element.isolateScope().textures;
+    expect(textures.length).toBe(2);
+    expect(textures[0].src).toBe('foo');
+    expect(textures[1].src).toBe('bar');
+  });
+
+  it('records selected texture url', function() {
+    var element = $compile(
+      '<div texturegroup ng-model="texUrl">'+
+        '<texture src="foo"></texture><texture selected=1 src="bar"></texture>' +
+        '<texture src="buzz"></texture>' +
+      '</div>')($rootScope);
+    $rootScope.$digest();
+
+    var textures = element.isolateScope().textures;
+    expect(textures.length).toBe(3);
+    expect(textures[0].src).toBe('foo');
+    expect(textures[1].src).toBe('bar');
+    expect(textures[2].src).toBe('buzz');
+    expect($rootScope.texUrl).toBe('bar');
+  });
+
+  it('defaults to selecting first texture url', function() {
+    var element = $compile(
+      '<div texturegroup ng-model="texUrl">'+
+        '<texture src="foo"></texture><texture src="bar"></texture>' +
+        '<texture src="buzz"></texture>' +
+      '</div>')($rootScope);
+    $rootScope.$digest();
+
+    var textures = element.isolateScope().textures;
+    expect(textures.length).toBe(3);
+    expect(textures[0].src).toBe('foo');
+    expect(textures[1].src).toBe('bar');
+    expect(textures[2].src).toBe('buzz');
+    expect($rootScope.texUrl).toBe('foo');
   });
 });
