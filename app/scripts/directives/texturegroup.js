37d12a544d3159b2ec5c580c6cf023c1cff6fc6e
Rich Wareham <rjw57@cam.ac.uk>
2013-12-12 16:55:49 +0000

    stub in <texture> sub-directive for textures

--- app/scripts/directives/texturegroup.js
+++ app/scripts/directives/texturegroup.js
@@ -6,7 +6,23 @@ angular.module('stereogramApp')
       restrict: 'E',
       replace: false,
       scope: { },
-      template: '<div class="texture-group" ng-transclude></div>',
+      template:
+        '<div class="texture-group container">' +
+          '<div class="row" ng-transclude></div>' +
+        '</div>',
       transclude: true,
+    };
+  })
+  .directive('texture', function() {
+    return {
+      restrict: 'E',
+      scope: { src: '@' },
+      replace: false,
+      template:
+        '<div class="texture" ng-class="{selected: selected}">' +
+          '<a href="" ng-click="selected = true"><div class="texture-frame">' +
+            '<div class="texture-swatch" style="background-image: url({{ src }})">' +
+          '</div></a>' +
+        '</div>',
     };
   });
