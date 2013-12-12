6d58070cb30e8cc062b540f2163457d5242abd99
Rich Wareham <rjw57@cam.ac.uk>
2013-12-12 15:28:51 +0000

    added in stereogram engine

--- app/scripts/services/stereogram.js
+++ app/scripts/services/stereogram.js
@@ -4,6 +4,41 @@ angular.module('stereogramApp')
 
     service.renderIntoImageData = function(depthData, textureData, destData, options) {
       options = angular.extend({ maxDepthOffset: 64 }, options);
+
+      var maxDepthOffset = options.maxDepthOffset;
+
+      // Calculate left-most margin
+      var leftMargin = Math.max(textureData.width, maxDepthOffset);
+
+      var i;
+
+      // Generate destData
+      for(var row_idx=0; row_idx<destData.height; ++row_idx) {
+        var texRowOffset = (row_idx % textureData.height) * 4 * textureData.width;
+        var depthRowOffset = (row_idx % depthData.height) * 4 * depthData.width;
+        var destRowOffset = row_idx * 4 * destData.width;
+
+        for(var col_idx=0; col_idx<destData.width; ++col_idx) {
+          if(col_idx < leftMargin) {
+            for(i=0; i<4; ++i) {
+              destData.data[destRowOffset + col_idx*4 + i] =
+                textureData.data[texRowOffset + (col_idx % textureData.width)*4 + i];
+            }
+          } else {
+            var depth = depthData.data[
+              depthRowOffset + ((col_idx - leftMargin) % depthData.width)*4];
+
+            // Rescale depth from 0 -> maxDepthOffset
+            depth *= maxDepthOffset / 255;
+
+            var offset = Math.floor(leftMargin - depth);
+            for(i=0; i<4; ++i) {
+              destData.data[destRowOffset + col_idx*4 + i] =
+                destData.data[destRowOffset + (col_idx - offset)*4 + i];
+            }
+          }
+        }
+      }
     };
 
     return service;
--- test/spec/services/stereogram.js
+++ test/spec/services/stereogram.js
@@ -4,6 +4,17 @@ describe('Service: stereogram', function () {
   // load the services's module
   beforeEach(module('stereogramApp'));
 
+  var createImageData = function(width, height) {
+    var canvas = document.createElement('canvas');
+    var ctx = canvas.getContext('2d');
+    var data = ctx.createImageData(width, height);
+
+    // Initialise data to 0x70
+    for(var i in data.data) { data[i] = 0x70; }
+
+    return data;
+  };
+
   var stereogram;
 
   // Initialize the service
@@ -17,5 +28,37 @@ describe('Service: stereogram', function () {
 
   it('should export renderIntoImageData function', function() {
     expect(stereogram.renderIntoImageData).not.toBeUndefined();
+  });
+
+  it('should accept three image data instances', function() {
+    var
+      depth = createImageData(640, 480),
+      tex = createImageData(60,60),
+      dest = createImageData(640,480);
+
+    expect(depth).toBeTruthy();
+    expect(tex).toBeTruthy();
+    expect(dest).toBeTruthy();
+
+    stereogram.renderIntoImageData(depth, tex, dest);
+  }); 
+
+  it('should only write data from texture image to output', function() {
+    var
+      depth = createImageData(40, 40),
+      tex = createImageData(10,10),
+      dest = createImageData(40,40),
+      i;
+
+    for(i in tex.data) {
+      tex.data[i] = 10 + (i % 10);
+    }
+
+    stereogram.renderIntoImageData(depth, tex, dest);
+
+    for(i in dest.data) {
+      expect(dest.data[i]).toBeGreaterThan(9);
+      expect(dest.data[i]).toBeLessThan(20);
+    }
   });
 });
